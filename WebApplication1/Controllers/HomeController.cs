6ea75ca15c7e6de00065381dc0b6aa8cb9f5048f
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-09-22 15:59:39 +0200

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IAsyncResult Upload()
+        {
+            string storageConnection = CloudConfigurationManager.GetSetting("BlobStorageConnectionString"); CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+
+            //create a block blob
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            //create a container 
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("appcontainer");
+
+            //create a container if it is not already exists
+
+            if (await cloudBlobContainer.CreateIfNotExistsAsync())
+            {
+
+                await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            }
+
+
+            string imageName = "Test-" + Path.GetExtension(imageToUpload.FileName);
+
+            //get Blob reference
+
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(imageName); cloudBlockBlob.Properties.ContentType = imageToUpload.ContentType;
+
+            await cloudBlockBlob.UploadFromStreamAsync(imageToUpload.InputStream);
+            return View();
+        }
+
+        public IActionResult Download()
+        {
+            var containerName = "testcontainerherbi";
+
+            string storageConnection = CloudConfigurationManager.GetSetting("BlobStorageConnectionString"); CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection); CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+
+
+            CloudBlobContainer cloudBlobContainer = blobClient.GetContainerReference(containerName); CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference("uploadedfilename.ext");
+
+
+
+            MemoryStream memStream = new MemoryStream();
+
+            blockBlob.DownloadToStream(memStream);
+
+            HttpContext.Current.Response.ContentType = blockBlob.Properties.ContentType.ToString(); HttpContext.Current.Response.AddHeader("Content-Disposition", "Attachment; filename=" + blockBlob.ToString());
+
+            HttpContext.Current.Response.AddHeader("Content-Length", blockBlob.Properties.Length.ToString()); HttpContext.Current.Response.BinaryWrite(memStream.ToArray()); HttpContext.Current.Response.Flush(); HttpContext.Current.Response.Close();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async void Get()
+        {
+            var containerName = "testcontainerherbi";
+
+            string storageConnection = CloudConfigurationManager.GetSetting("DefaultEndpointsProtocol=https;AccountName=dropbox1;AccountKey=S3LxIjH+HG0c0GgZgdzAGytdj1vHXvGk75g0iauv0StSjtVGT3c8j6I6741s9cW7B32CgI7RENCwnX5va+r67A==;EndpointSuffix=core.windows.net");
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+            CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+
+
+            CloudBlobContainer cloudBlobContainer = blobClient.GetContainerReference(containerName);
+            CloudBlockBlob blockBlob = cloudBlobContainer.GetBlockBlobReference("uploadedfilename.ext");
+
+
+
+            MemoryStream memStream = new MemoryStream();
+
+            await blockBlob.DownloadToStreamAsync(memStream);
+
+            var a = memStream.ToArray();
+
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async void Post([FromBody]string imageToUpload)
+        {
+            string storageConnection = CloudConfigurationManager.GetSetting("DefaultEndpointsProtocol=https;AccountName=dropbox1;AccountKey=S3LxIjH+HG0c0GgZgdzAGytdj1vHXvGk75g0iauv0StSjtVGT3c8j6I6741s9cW7B32CgI7RENCwnX5va+r67A==;EndpointSuffix=core.windows.net");
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+
+            //create a block blob
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            //create a container 
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("dropbox");
+
+            //create a container if it is not already exists
+
+            if (await cloudBlobContainer.CreateIfNotExistsAsync())
+            {
+
+                await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            }
+
+
+            string imageName = "Test-" + Path.GetExtension(imageToUpload.FileName);
+
+            //get Blob reference
+
+            CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(imageName); cloudBlockBlob.Properties.ContentType = imageToUpload.ContentType;
+
+            await cloudBlockBlob.UploadFromStreamAsync(imageToUpload.InputStream);
+        }
+
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
